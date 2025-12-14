31c12759e41762b79671a8feea2acddfc80ce3c3
Peter Nzingu <peterkyalo69@gmail.com>
2025-12-14 15:15:10 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/layout/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>This is App component!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <h1>This is a header component in React</h1>;
+}
+
+export default Header;
