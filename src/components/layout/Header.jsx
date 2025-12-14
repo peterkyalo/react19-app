4aeb8efb88380b0260f6d7b96d3cd7b835222f7e
Peter Nzingu <peterkyalo69@gmail.com>
2025-12-14 15:31:51 +0300

    Add Button component and update Header to include buttons

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,13 @@
 import { Naviagation } from "./Naviagation";
+import { Button } from "./ui/Button";
 
 function Header() {
   return (
     <>
       <h1>This is Header component!</h1>
       <Naviagation />
+      <Button />
+      <Button label="Submit" />
     </>
   );
 }
