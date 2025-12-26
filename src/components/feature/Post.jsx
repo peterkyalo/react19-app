6ef2e06d385ec0e0dda4987f14cfc05f2b47282e
Peter Nzingu <peterkyalo69@gmail.com>
2025-12-26 18:00:59 +0300

    Enhance Posts component with search functionality and timer display

--- src/components/feature/Post.jsx
+++ src/components/feature/Post.jsx
@@ -2,20 +2,46 @@ import { useEffect, useState } from "react";
 export const Posts = () => {
   // State to hold posts data
   const [posts, setPosts] = useState([]);
+  const [query, setQuery] = useState("");
+  const [seconds, setSeconds] = useState(0);
   // 1. Mounting Phase
   useEffect(() => {
     console.log("Mounting");
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    fetch(`https://jsonplaceholder.typicode.com/posts?q=${query}`)
       .then((res) => {
         if (!res.ok) throw new Error("Network response was not ok!");
         return res.json();
       })
       .then((data) => setPosts(data))
       .catch((err) => console.error("Fetch error:", err));
+  }, [query]);
+
+  // 3 Cleanup Phase
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+    // Cleanup function
+    return () => clearInterval(timer);
   }, []);
+
+  // Set post search
+  const changeHandler = (e) => {
+    // console.log(e.target.value);
+    setQuery(e.target.value);
+  };
+
   return (
     <div>
+      <div>
+        <div>Timer: {seconds} seconds</div>
+      </div>
       <h4>Post Listing</h4>
+      <input
+        type="text"
+        onChange={changeHandler}
+        placeholder="Search posts..."
+      />
       <table>
         <thead>
           <tr>
@@ -36,7 +62,11 @@ export const Posts = () => {
               ))}
             </>
           ) : (
-            <p>No posts...</p>
+            <tr>
+              <td colSpan="3">
+                <p>No posts...</p>
+              </td>
+            </tr>
           )}
         </tbody>
       </table>
