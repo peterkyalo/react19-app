46b6e4aa104c837e1a947131f2a3e930f7fda86a
Peter Nzingu <peterkyalo69@gmail.com>
2025-12-26 17:36:28 +0300

    Add Posts component to fetch and display post listings

--- src/components/feature/Post.jsx
+++ src/components/feature/Post.jsx
@@ -1,5 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 export const Posts = () => {
+  // State to hold posts data
+  const [posts, setPosts] = useState([]);
   // 1. Mounting Phase
   useEffect(() => {
     console.log("Mounting");
@@ -8,12 +10,36 @@ export const Posts = () => {
         if (!res.ok) throw new Error("Network response was not ok!");
         return res.json();
       })
-      .then((data) => console.log("posts", data))
+      .then((data) => setPosts(data))
       .catch((err) => console.error("Fetch error:", err));
   }, []);
   return (
     <div>
       <h4>Post Listing</h4>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.length > 0 ? (
+            <>
+              {posts.map((post) => (
+                <tr>
+                  <td>{post.id}</td>
+                  <td>{post.title}</td>
+                  <td>{post.body}</td>
+                </tr>
+              ))}
+            </>
+          ) : (
+            <p>No posts...</p>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
